99b91a60dccc599511b220c8dd054cac273b8c38
0GiS0 <giselatb@outlook.com>
2024-03-06 23:28:40 +0100

    Add network connection and attached network creation

--- scripts/09-create-network-connections.sh
+++ scripts/09-create-network-connections.sh
@@ -1,11 +1,18 @@
 echo -e "Create network connection"
 
-az devcenter admin network-connection create \
+NETWORK_CONNECTION_ID=$(az devcenter admin network-connection create \
 --name "$VNET_NAME-connection" \
 --resource-group $RESOURCE_GROUP \
 --location $LOCATION \
 --domain-join-type "AzureAdJoin" \
 --networking-resource-group-name $RESOURCE_GROUP-vnet \
---subnet-id $(az network vnet subnet show --name $SUBNET_NAME --vnet-name $VNET_NAME --resource-group $RESOURCE_GROUP --query id -o tsv)
+--subnet-id $(az network vnet subnet show --name $SUBNET_NAME --vnet-name $VNET_NAME --resource-group $RESOURCE_GROUP --query id -o tsv) \
+--query id -o tsv)
 
-az devcenter admin network-connection list --resource-group $RESOURCE_GROUP -o table
+# az devcenter admin network-connection list --resource-group $RESOURCE_GROUP -o table
+
+az devcenter admin attached-network create \
+--name "$VNET_NAME-attached-network" \
+--resource-group $RESOURCE_GROUP \
+--dev-center $DEV_CENTER_NAME \
+--network-connection-id $NETWORK_CONNECTION_ID
--- scripts/10-create-dev-box-pool.sh
+++ scripts/10-create-dev-box-pool.sh
@@ -1,10 +1,10 @@
 echo -e "Create Dev Box pool $DEV_BOX_POOL_NAME"
 
-az devcenter admin pool create \
---name $DEV_BOX_POOL_NAME \
+time az devcenter admin pool create \
+--name "${DEV_BOX_POOL_NAME}_with_vnet" \
 --project-name $DEV_CENTER_PROJECT \
 --resource-group $RESOURCE_GROUP \
 --devbox-definition-name $DEV_BOX_DEFINITION_NAME \
 --local-administrator Enabled \
---virtual-network-type Managed \
---managed-virtual-network-regions $LOCATION
+--virtual-network-type Unmanaged \
+--network-connection-name "$VNET_NAME-attached-network" 
