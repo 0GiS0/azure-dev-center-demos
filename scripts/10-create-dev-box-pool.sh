ad68fe29e4e3539f951a5e9e698ed978290b6c48
0GiS0 <giselatb@outlook.com>
2024-03-07 17:13:53 +0100

    Update devbox-definition-name to use a default image

--- scripts/10-create-dev-box-pool.sh
+++ scripts/10-create-dev-box-pool.sh
@@ -4,7 +4,7 @@ time az devcenter admin pool create \
 --name "${DEV_BOX_POOL_NAME}_with_vnet" \
 --project-name $DEV_CENTER_PROJECT \
 --resource-group $RESOURCE_GROUP \
---devbox-definition-name $DEV_BOX_DEFINITION_NAME \
+--devbox-definition-name $DEV_BOX_DEFINITION_NAME_FOR_A_DEFAULT_IMAGE \
 --local-administrator Enabled \
 --virtual-network-type Unmanaged \
 --network-connection-name "$VNET_NAME-attached-network" 
