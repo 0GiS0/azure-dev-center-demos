ba4e6873545ad986cf581a10950d60966bc802c0
0GiS0 <giselatb@outlook.com>
2024-03-07 23:47:24 +0100

    Add ARM templates and environment configuration files

--- scripts/0-minimal-setup/02-create-dev-center.sh
+++ scripts/0-minimal-setup/02-create-dev-center.sh
@@ -12,4 +12,9 @@ DEV_CENTER_CLIENT_ID=$(az devcenter admin devcenter show \
 --resource-group $RESOURCE_GROUP \
 --query identity.principalId -o tsv)
 
-
+# Assign the Contributor role to the Dev Center
+# If you don't have the role, you can't deploy environments
+az role assignment create \
+--role "Contributor" \
+--assignee $DEV_CENTER_CLIENT_ID \
+--scope /subscriptions/$SUBSCRIPTION_ID
--- scripts/04-environments/01-create-a-catalog.sh
+++ scripts/04-environments/01-create-a-catalog.sh
@@ -38,6 +38,39 @@ az devcenter admin catalog get-sync-error-detail \
 --resource-group $RESOURCE_GROUP
 
 az devcenter admin catalog show \
---name quickstart \
+--name $CATALOG_NAME \
 --dev-center $DEV_CENTER_NAME \
 --resource-group $RESOURCE_GROUP
+
+# Get secret using URI
+# az keyvault secret show --id $SECRET_ID --query value -o tsv
+
+# Create a project for the catalog
+az devcenter admin project create \
+--name "tour-of-heroes-env" \
+--dev-center $DEV_CENTER_ID \
+--resource-group $RESOURCE_GROUP
+
+echo -e "Give access to your developers to this project" 
+
+az role assignment create \
+--role "Deployment Environments User" \
+--assignee $USER_EMAIL \
+--scope $(az devcenter admin project show --name "tour-of-heroes-env" --resource-group $RESOURCE_GROUP --query id -o tsv)
+
+# Create environment type
+az devcenter admin environment-type create \
+--name "dev" \
+--dev-center $DEV_CENTER_NAME \
+--resource-group $RESOURCE_GROUP
+
+# Assign the environment type to the project
+az devcenter admin project-environment-type create \
+--name "dev" \
+--dev-center $DEV_CENTER_NAME \
+--resource-group $RESOURCE_GROUP \
+--project-name "tour-of-heroes-env" \
+--roles "Contributor" \
+--identity-type "SystemAssigned" \
+--deployment-target-id $SUBSCRIPTION_ID \
+--status "Enabled"
