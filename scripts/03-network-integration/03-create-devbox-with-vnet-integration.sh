2a1e2e3b94a330491410db7b8329d0b8663ff28a
0GiS0 <giselatb@outlook.com>
2024-03-12 18:36:15 +0100

    Update dev box creation and pool creation scripts

--- scripts/03-network-integration/03-create-devbox-with-vnet-integration.sh
+++ scripts/03-network-integration/03-create-devbox-with-vnet-integration.sh
@@ -1,16 +1,16 @@
+image_name=${image_names[1]}
+
 IMAGE_REFERENCE_ID=$(az devcenter admin image show \
 --resource-group $RESOURCE_GROUP \
 --dev-center $DEV_CENTER_NAME \
---gallery-name Default \
---name  "microsoftwindowsdesktop_windows-ent-cpc_win11-22h2-ent-cpc-m365" \
+--gallery-name $PACKER_GALLERY_NAME \
+--name  $image_name \
 --query id -o tsv)
 
-echo -e "You can get all skus to see how many CPU, memmory and XX you want"
-
-az devcenter admin sku list -o table
+DEV_BOX_DEFINITION_FOR_VNET_INTEGRATION="vsbox-vnet"
 
 time az devcenter admin devbox-definition create \
---name $DEV_BOX_DEFINITION_NAME_FOR_A_DEFAULT_IMAGE \
+--name $DEV_BOX_DEFINITION_FOR_VNET_INTEGRATION \
 --dev-center $DEV_CENTER_NAME \
 --resource-group $RESOURCE_GROUP \
 --image-reference id=$IMAGE_REFERENCE_ID \
--- scripts/03-network-integration/04-create-dev-box-pool.sh
+++ scripts/03-network-integration/04-create-dev-box-pool.sh
@@ -1,10 +1,14 @@
+project_name=${projects_names[1]}
 
+DEV_BOX_POOL_WITH_VNET_INTEGRATIOn="pool_with_vnet"
+
+echo -e "Creating a dev box pool with vnet integration for project $project_name"
 
 time az devcenter admin pool create \
---name "${DEV_BOX_POOL_NAME}_with_vnet" \
+--name $DEV_BOX_POOL_WITH_VNET_INTEGRATIOn \
 --project-name $project_name \
 --resource-group $RESOURCE_GROUP \
---devbox-definition-name $DEV_BOX_DEFINITION_NAME_FOR_A_DEFAULT_IMAGE \
+--devbox-definition-name $DEV_BOX_DEFINITION_FOR_VNET_INTEGRATION \
 --local-administrator Enabled \
 --virtual-network-type Unmanaged \
 --network-connection-name "$VNET_NAME-attached-network" 
