4e3a3a7280a35929fcbb15a63e69ca8334183689
0GiS0 <giselatb@outlook.com>
2024-04-20 20:31:59 +0200

    Update dev center environment cleanup script

--- scripts/clean-up.sh
+++ scripts/clean-up.sh
@@ -34,21 +34,22 @@
 # --project-name qaenv
 
 
-# az devcenter admin environment-type list \
-# --resource-group $RESOURCE_GROUP \
-# --dev-center $DEV_CENTER_NAME \
-# -o table
+az devcenter admin environment-type list \
+--resource-group $RESOURCE_GROUP \
+--dev-center $DEV_CENTER_NAME \
+-o table
 
 # # Delete environments before deleting the dev center
-# az devcenter admin project-environment-type delete \
-# --name "test" \
-# --resource-group $RESOURCE_GROUP --project-name "tour-of-heroes-environments" 
+az devcenter admin project-environment-type delete \
+--name "dev" \
+--resource-group $RESOURCE_GROUP \
+--project-name "tour-of-heroes-environments" 
 
 # # Check environments
-# az devcenter dev environment list \
-# --project-name "tour-of-heroes-environments" \
-# --dev-center-name $DEV_CENTER_NAME \
-# --user-id "b73d8792-1dc4-407f-9be9-96f65f84e621"
+az devcenter dev environment list \
+--project-name "tour-of-heroes-environments" \
+--dev-center-name $DEV_CENTER_NAME \
+--user-id "b73d8792-1dc4-407f-9be9-96f65f84e621"
 
 time az group delete --name $RESOURCE_GROUP --yes
 
@@ -68,9 +69,9 @@ az role definition delete \
 # Check custom role
 az role definition list --custom-role-only true
 
-# az devcenter dev environment list \
-# --project-name "tour-of-heroes-environments" \
-# --dev-center-name $DEV_CENTER_NAME
+az devcenter dev environment list \
+--project-name "tour-of-heroes-environments" \
+--dev-center-name $DEV_CENTER_NAME
 
 # az devcenter dev environment delete \
 # --name "web-app-using-tf" \
