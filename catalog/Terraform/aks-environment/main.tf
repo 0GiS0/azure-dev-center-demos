01b44c6968f9bdc0b0bf6c4ac4fef5e5e9244e1a
0GiS0 <giselatb@outlook.com>
2024-04-20 08:40:48 +0200

    Add Terraform files for creating an AKS environment

---
+++ catalog/Terraform/aks-environment/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+  }
+
+  required_version = ">= 1.0.0"
+}
+
+provider "azurerm" {
+  features {}
+
+  skip_provider_registration = true
+}
+
+variable "resource_group_name" {}
+
+variable "resource_name" {}
+
+variable "location" {}
+
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.resource_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = var.resource_name
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Azure Global 2024"
+  }
+}
