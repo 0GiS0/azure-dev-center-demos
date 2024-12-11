908161b42cb3ecdd16bd06fd62eff41dfe9c400a
Raymond Kao <raykao@github.com>
2024-12-11 03:49:03 +0000

    fix(packer): contributor permission required to allow packer to create the image in RG

--- terraform/azurerm_role_assignment.tf
+++ terraform/azurerm_role_assignment.tf
@@ -16,6 +16,14 @@ resource "azurerm_role_assignment" "dev-center-sig" {
   principal_id       = azurerm_user_assigned_identity.dev-center.principal_id
 }
 
+resource "azurerm_role_assignment" "packer_dev_center_rg_contributor" {
+  description = "Allow Packer to view resources in the main Dev Center Resource Group (Used for Shared Compute Gallery)"
+  role_definition_name = "Contributor"
+  principal_type = "ServicePrincipal"
+  principal_id = azuread_service_principal.packer.object_id
+  scope = azurerm_resource_group.default.id
+}
+
 resource "azurerm_role_assignment" "packer_rg_contributor" {
   description = "Allow Packer to manage/deploy resources in the a specialized Packer build only Resource Group"
   role_definition_name = "Contributor"
