d2b86e4cfc02dbe41b98b8e9f16664fa3c1c16d4
Raymond Kao <raykao@github.com>
2024-12-11 16:24:38 +0000

    fix(dev-center-gallery): name can't contain dashes

--- terraform/modules/dev-center/azurerm_dev_center_gallery.tf
+++ terraform/modules/dev-center/azurerm_dev_center_gallery.tf
@@ -1,5 +1,5 @@
 resource "azurerm_dev_center_gallery" "default" {
-  name              = "dev-center-gallery"
+  name              = "devcentergallery"
   dev_center_id     = azurerm_dev_center.default.id
   shared_gallery_id = var.shared_gallery_id
 }
