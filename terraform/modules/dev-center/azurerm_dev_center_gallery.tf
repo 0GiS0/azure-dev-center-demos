5a458285411aaf37764e96b37c1979e1aec62395
Raymond Kao <raykao@github.com>
2024-12-11 15:38:59 +0000

    chore(dev-center-gallery): rename for generic use and clarity

--- terraform/modules/dev-center/azurerm_dev_center_gallery.tf
+++ terraform/modules/dev-center/azurerm_dev_center_gallery.tf
@@ -1,5 +1,5 @@
 resource "azurerm_dev_center_gallery" "default" {
-  name              = "gbbtest001"
+  name              = "dev-center-gallery"
   dev_center_id     = azurerm_dev_center.default.id
   shared_gallery_id = var.shared_gallery_id
 }
