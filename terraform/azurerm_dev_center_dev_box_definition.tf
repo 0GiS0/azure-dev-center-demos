de47d532f5b47688afc9fd7b2400439ed1eae6ea
Raymond Kao <raykao@github.com>
2024-12-11 03:53:56 +0000

    fix(devbox definition): image reference and SKU were wrong. image ref is for dev box gallery image name and sku is for VM not image

--- terraform/azurerm_dev_center_dev_box_definition.tf
+++ terraform/azurerm_dev_center_dev_box_definition.tf
@@ -1,7 +1,8 @@
 resource "azurerm_dev_center_dev_box_definition" "default" {
+  for_each = var.custom_images
   location           = azurerm_resource_group.default.location
-  name               = "default-dcet"
+  name               = "${each.key}"
   dev_center_id      = module.dev-center.id
-  image_reference_id = data.azurerm_shared_image_version.custom_images["jetbrains"].id
-  sku_name           = "demo"
+  image_reference_id = "${module.dev-center.gallery_id}/images/${azurerm_shared_image.image_definitions[each.key].name}"
+  sku_name           = "general_i_8c32gb256ssd_v2" # The VM SKU to use for the Dev Box NOT the image SKU
 }
