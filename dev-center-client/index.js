f5c5e57d4892b286c08f97cff9dcf313f316a950
0GiS0 <giselatb@outlook.com>
2025-01-26 14:03:59 +0000

    Add initial setup for dev-center-client with Express server and Azure integration

---
+++ dev-center-client/index.js
@@ -0,0 +1,134 @@
+const { DefaultAzureCredential } = require("@azure/identity");
+const { isUnexpected, getLongRunningPoller } = require("@azure-rest/developer-devcenter");
+const createClient = require("@azure-rest/developer-devcenter").default;
+require("dotenv").config();
+
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+
+// Get Projects
+app.get('/projects', async (req, res) => {
+    const endpoint = process.env.DEVCENTER_ENDPOINT || "<devcenter name>";
+    const client = createClient(endpoint, new DefaultAzureCredential());
+
+    console.log("Endpoint: ", endpoint);
+
+    const projectList = await client.path("/projects").get();
+    if (isUnexpected(projectList)) {
+        throw new Error(projectList.body.error);
+    }
+
+    console.log("Projects: ", projectList.body.value);
+
+    res.send(projectList.body.value);
+});
+
+//Get DevBoxs for the user logged in
+app.get('/devboxes', async (req, res) => {
+    const endpoint = process.env.DEVCENTER_ENDPOINT || "<devcenter name>";
+    const client = createClient(endpoint, new DefaultAzureCredential());
+
+    console.log("Endpoint: ", endpoint);
+
+    const devBoxList = await client.path("/devboxes").get();
+    if (isUnexpected(devBoxList)) {
+        throw new Error(devBoxList.body.error);
+    }
+
+    console.log("DevBoxes: ", devBoxList.body.value);
+
+    res.send(devBoxList.body.value);
+});
+
+// Get DevBoxes for a specific user
+app.get('/devboxes/:userId', async (req, res) => {
+    const endpoint = process.env.DEVCENTER_ENDPOINT || "<devcenter name>";
+    const client = createClient(endpoint, new DefaultAzureCredential());
+
+    console.log("Endpoint: ", endpoint);
+    console.log("UserId: ", req.params.userId);
+
+    console.log("/users/{userId}/devboxes", req.params.userId);
+
+    const devBoxList = await client.path(`/users/${req.params.userId}/devboxes`).get();
+    if (isUnexpected(devBoxList)) {
+        throw new Error(devBoxList.body.error);
+    }
+
+    console.log("DevBoxes: ", devBoxList.body.value);
+
+    res.send(devBoxList.body.value);
+});
+
+
+// Create a DevBox for a specific user
+app.post('/devboxes/:projectName/:userId', async (req, res) => {
+    const endpoint = process.env.DEVCENTER_ENDPOINT || "<devcenter name>";
+    const client = createClient(endpoint, new DefaultAzureCredential());
+
+    console.log("Endpoint: ", endpoint);
+    console.log("UserId: ", req.params.userId);
+    console.log("ProjectName: ", req.params.projectName);
+
+    // Get the pool for the project
+    const poolList = await client.path("/projects/{projectName}/pools", req.params.projectName).get();
+    if (isUnexpected(poolList)) {
+        throw new Error(poolList.body.error);
+    }
+
+    let pool = poolList.body.value[0];
+    if (pool === undefined || pool.name === undefined) {
+        throw new Error("No pools found.");
+    }
+
+    const devBoxCreateParameters = {
+        contentType: "application/json",
+        body: { poolName: pool.name },
+    };
+
+    // Provision a dev box
+    const devBoxCreateResponse = await client
+        .path(
+            "/projects/{projectName}/users/{userId}/devboxes/{devBoxName}",
+            req.params.projectName,
+            req.params.userId,
+            "devbox-created-by-api",
+        )
+        .put(devBoxCreateParameters);
+
+    if (isUnexpected(devBoxCreateResponse)) {
+        throw new Error(devBoxCreateResponse.body.error.message);
+    }
+
+    const devBoxCreatePoller = await getLongRunningPoller(client, devBoxCreateResponse);
+    const devBoxCreateResult = await devBoxCreatePoller.pollUntilDone();
+
+    console.log(`Provisioned dev box with state ${devBoxCreateResult.body.provisioningState}.`);
+
+
+    const remoteConnectionResult = await client
+        .path(
+            "/projects/{projectName}/users/{userId}/devboxes/{devBoxName}/remoteConnection",
+            req.params.projectName,
+            req.params.userId,
+            "devbox-created-by-api",
+        )
+        .get();
+
+    if (isUnexpected(remoteConnectionResult)) {
+        throw new Error(remoteConnectionResult.body.error.message);
+    }
+
+    console.log(`Connect using remote connection URL ${remoteConnectionResult.body.webUrl}.`);
+
+});
