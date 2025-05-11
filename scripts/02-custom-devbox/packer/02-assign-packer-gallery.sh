4e4c39f7c6c53163c4c1eee499dec4879232a1b5
0GiS0 <giselatb@outlook.com>
2025-05-11 20:30:31 +0000

    Refactor output messages in the assign packer gallery script to enhance user feedback

--- scripts/02-custom-devbox/packer/02-assign-packer-gallery.sh
+++ scripts/02-custom-devbox/packer/02-assign-packer-gallery.sh
@@ -1,20 +1,20 @@
 DEV_CENTER_CLIENT_ID=$(az devcenter admin devcenter show \
---name $DEV_CENTER_NAME \
---resource-group $RESOURCE_GROUP \
---query identity.principalId -o tsv)
+    --name $DEV_CENTER_NAME \
+    --resource-group $RESOURCE_GROUP \
+    --query identity.principalId -o tsv)
 
-echo "Let's assign the Contributor role to the Dev Center for the gallery"
+gum style --foreground 212 "🔑 Assigning Contributor role to Dev Center for the gallery..."
 
 az role assignment create \
---role "Contributor" \
---assignee $DEV_CENTER_CLIENT_ID \
---scope $(az sig show --gallery-name $PACKER_GALLERY_NAME \
---resource-group $PACKER_GALLERY_RESOURCE_GROUP --query id -o tsv)
+    --role "Contributor" \
+    --assignee $DEV_CENTER_CLIENT_ID \
+    --scope $(az sig show --gallery-name $PACKER_GALLERY_NAME \
+        --resource-group $PACKER_GALLERY_RESOURCE_GROUP --query id -o tsv)
 
-echo "Then you can associate the gallery with the Dev Center"
+gum style --foreground 35 "🖇️ Associating the gallery with the Dev Center..."
 
 az devcenter admin gallery create \
---name $PACKER_GALLERY_NAME \
---gallery-resource-id $(az sig show --gallery-name $PACKER_GALLERY_NAME --resource-group $PACKER_GALLERY_RESOURCE_GROUP --query id -o tsv) \
---dev-center $DEV_CENTER_NAME \
---resource-group $RESOURCE_GROUP
+    --name $PACKER_GALLERY_NAME \
+    --gallery-resource-id $(az sig show --gallery-name $PACKER_GALLERY_NAME --resource-group $PACKER_GALLERY_RESOURCE_GROUP --query id -o tsv) \
+    --dev-center $DEV_CENTER_NAME \
+    --resource-group $RESOURCE_GROUP
